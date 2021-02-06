cbd54be2af9d1fd018285cee44bab2880033fa1f
onokt <65003977+onokt@users.noreply.github.com>
2021-02-07 00:33:44 +0900

    Add files via upload

--- analysis_sabun.py
+++ analysis_sabun.py
@@ -1,9 +1,14 @@
 import cv2
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 
+os.makedirs('./Image/Measure')
+os.makedirs('./Image/Measure2')
+# os.makedirs('./Data/Scan')  #改善したい
+
 bg = cv2.imread('./Image/background.jpg')
-cap = cv2.VideoCapture('./image/chamber.mp4')
+cap = cv2.VideoCapture('./Image/discharge.MOV')
 # number = len(str(int(cap.get(cv2.CAP_PROP_FRAME_COUNT))))
 # model = cv2.bgsegm.createBackgroundSubtractorMOG()
 
@@ -19,49 +24,95 @@ while True:
     mask = model.apply(frame)
 
     cnt = cv2.countNonZero(mask)
-    if cnt > 1500 and cnt < 1600:  #threshold(1)　ここでthresholdをきつくすれば1層しかならないときのものをきれるはず
+    if cnt > 200 and cnt < 1600:  #threshold(1)　ここでthresholdをきつくすれば1層しかならないときのものをきれるはず
         cv2.imwrite('./Image/Measure/frame{}.jpg'.format(str(n)), frame)  #異常のあるフレーム(光っている)を保存
     # cv2.imwrite('/Image/Measure2/mask_image{}.jpg'.format(str(n)), output)
+
         img = cv2.imread('./Image/Measure/frame{}.jpg'.format(str(n)))
         model2 = cv2.bgsegm.createBackgroundSubtractorMOG()
         mask2 = model.apply(bg)
         mask2 = model.apply(img)
         cv2.imwrite('./Image/Measure2/mask_image{}.jpg'.format(str(n)), mask2)  #maskを保存(はじめのほうで保存できないのはなぜ？)
 
-        Y, X = mask2.shape
+#######################################################################################################################
 
-        R = []  # 画素値がゼロでないエリアのｙ座標
+        #x_max y_max の取得
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        Y, X = gray.shape
+
+        y_max = []  # 画素値がゼロでないエリアのｙ座標
         for j in range(Y):
             x = []  # x座標
             s = []  # 明るさ
             for i in range(X):
                 x.append(i)
-                s.append(int(mask2[j, i]))
+                s.append(int(gray[j, i]))
             print(s)
-            if s.count(255) > 20:  # threshold(2) (ここのthresholdはさじ加減大事)
-                R.append(j)
+            if max(s) > 100:  # threshold(2) (ここのthresholdはさじ加減大事)
+                # fig = plt.figure()
+                # plt.plot(x, s)
+                # fig.savefig("./Data/Scan/img{}.png".format(str(j)))  # j = y座標
+                y_max.append(j)
 
         # print(R)
         # print(len(R))
-        y_max = []  #光っているy座標
         x_max = []  #光っている位置xのうち画素値が最大のx座標
-        for y2 in R:
-            x1 = []
+        for y2 in y_max:
             s1 = []
             for m in range(X):
-                x1.append(m)
-                s1.append(int(mask2[y2, m]))
-            y_max.append(y2)
+                s1.append(int(gray[y2, m]))
             x_max.append((s1.index(max(s1))))
-            # plt.plot(x1, 1)
+            # plt.plot(x1, y1)
             # plt.show()
 
-        # print(str(x_max)+' '+str(y_max))
+#####################################################################################################################
+        # 各座標の平均を求める
+        ave_x1_x2_x3 = []  #平均:　放電の数だけx座標が入る
+        ave_y1_y2_y3 = []
+        r = 0  #連続部の数　（一か所平均が求まるとリセットされる）
+        j = 0  #while文を回すためのindex
+        Sum_x = 0
+        Sum_y = 0
+        while True:
+            if j == len(y_max) - 1:
+                Sum_x += x_max[j]
+                Sum_y += y_max[j]
+                ave_x1_x2_x3.append(round((Sum_x / (r + 1)), 1))
+                ave_y1_y2_y3.append(round((Sum_y / (r + 1)), 1))
+                break
+            if y_max[j + 1] == y_max[j] + 1:
+                Sum_x += x_max[j]
+                Sum_y += y_max[j]
+                r += 1
+            elif y_max[j + 1] > y_max[j] + 1:
+                Sum_x += x_max[j]
+                Sum_y += y_max[j]
+                ave_x1_x2_x3.append(round((Sum_x / (r + 1)), 1))
+                ave_y1_y2_y3.append(round((Sum_y / (r + 1)), 1))
+                Sum_x = 0
+                Sum_y = 0
+                r = 0
 
-        file = open(f'./Data/list{str(n)}.txt', 'w')
-        for i in range(int(len(x_max))):
-            file.write(str(x_max[i])+' '+str(-y_max[i])+'\n')
+            j += 1
+
+        # print(ave_x1_x2_x3)
+        # print(ave_y1_y2_y3)
+##################################################################################################################
+
+        file = open(f'./Data/Spark_coordinate.txt', 'a')
+        file.write(str(ave_x1_x2_x3)+'\n')
             # print(len(x_max))
         file.close()
+
+
+
+
+
+
+        # file = open(f'./Data/list{str(n)}.txt', 'w')
+        # for i in range(int(len(x_max))):
+        #     file.write(str(x_max[i])+' '+str(-y_max[i])+'\n')
+        #     # print(len(x_max))
+        # file.close()
         n += 1
 
